c2e0eaf919b0ebaf3929471d23edb231bdd00d59
Emily Ng <ngemily2@gmail.com>
2016-02-20 12:29:39 -0500

    add WLOG, ELOG; include new line in debug strings

--- src/debug.h
+++ src/debug.h
@@ -2,10 +2,23 @@
 #define __DEBUG_H
 
 #define DEBUG 1
+
+
 #if DEBUG
-#define DLOG(fmt, ...) printf("INFO: <%s> " fmt, __FUNCTION__, ##__VA_ARGS__)
+
+#define DLOG(fmt, ...) printf("INFO: <%s> " fmt "\n",\
+        __FUNCTION__, ##__VA_ARGS__)
+#define WLOG(fmt, ...) printf("WARNING: <%s> " fmt "\n",\
+        __FUNCTION__, ##__VA_ARGS__)
+#define ELOG(fmt, ...) printf("ERROR: <%s> " fmt "\n",\
+        __FUNCTION__, ##__VA_ARGS__)
+
 #else
+
 #define DLOG(fmt, ...)
+#define WLOG(fmt, ...)
+#define ELOG(fmt, ...)
+
 #endif
 
 #endif
