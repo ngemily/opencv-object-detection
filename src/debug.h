7ebb663ac5ec7a7bced70b7ce9d9569bbdf58bd3
Emily Ng <ngemily2@gmail.com>
2016-03-07 15:42:07 -0500

    always print info, warning, and error messages; print debug only if flag defined

--- src/debug.h
+++ src/debug.h
@@ -1,24 +1,19 @@
 #ifndef __DEBUG_H
 #define __DEBUG_H
 
-#define DEBUG 1
 
-
-#if DEBUG
-
-#define DLOG(fmt, ...) printf("INFO: <%s> " fmt "\n",\
+#define ILOG(fmt, ...) printf("INFO: <%s> " fmt "\n",\
         __FUNCTION__, ##__VA_ARGS__)
 #define WLOG(fmt, ...) printf("WARNING: <%s> " fmt "\n",\
         __FUNCTION__, ##__VA_ARGS__)
 #define ELOG(fmt, ...) printf("ERROR: <%s> " fmt "\n",\
         __FUNCTION__, ##__VA_ARGS__)
 
+#if DEBUG
+#define DLOG(fmt, ...) printf("DEBUG: <%s> " fmt "\n",\
+        __FUNCTION__, ##__VA_ARGS__)
 #else
-
 #define DLOG(fmt, ...)
-#define WLOG(fmt, ...)
-#define ELOG(fmt, ...)
-
 #endif
 
 #endif
